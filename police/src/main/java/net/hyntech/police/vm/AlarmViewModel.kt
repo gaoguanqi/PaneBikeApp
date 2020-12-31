85eec49d5d61a7048554b61cfdef905c99335770
高冠祺 <307590625@qq.com>
2020-12-31 19:19:56 +0800

    alarm

--- police/src/main/java/net/hyntech/police/vm/AlarmViewModel.kt
+++ police/src/main/java/net/hyntech/police/vm/AlarmViewModel.kt
@@ -2,6 +2,7 @@ package net.hyntech.police.vm
 
 import net.hyntech.baselib.base.BaseViewModel
 import net.hyntech.common.model.repository.CommonRepository
+import java.util.*
 
 class AlarmViewModel:BaseViewModel() {
 
@@ -12,8 +13,21 @@ class AlarmViewModel:BaseViewModel() {
     var lastPage: Boolean = true
 
 
-    fun getAlarmInfoList() {
+    /**
+     * | 字段        |类型  |必须 |描述| 取值 |
+    | --------   | -----  | :---- |:----  |
+    | messageId     | String|  否  |消息id|-|
+    | messageType     | String|  否  |消息类型|alarm车主报警，move位移变化，enclosure围栏报警|
+     */
+    fun getAlarmInfoList(messageId:String,messageType:String) {
+        launchOnlyResult({
+            val params: WeakHashMap<String, Any> = WeakHashMap()
+            params.put("messageId",messageId)
+            params.put("messageType",messageType)
+            repository.searchDecive(params)
+        }, success = {
 
+        })
     }
 
 }
